2a3df97c31ae57be3373383dc71692085a74577f
Qui Nguyen <qui@gmail.com>
2024-02-29 22:19:06 -0500

    first commit

---
+++ src/components/BookList.jsx
@@ -0,0 +1,14 @@
+const BookList = () => {
+  return (
+    <div className="book-list">
+      <ul>
+        <li>To Kill a Mockingbird</li>
+        <li>Don Quixote</li>
+        <li>War and Peace</li>
+        <li>Gone with the Wind</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Context App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
