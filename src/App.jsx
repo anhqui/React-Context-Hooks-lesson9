11c0ba1188e7487e21ae1454df29637d44d5f83b
Qui Nguyen <qui@gmail.com>
2024-03-01 21:47:52 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useState, createContext } from "react";
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 
+export const ThemeContext = createContext();
+
 function App() {
+  const [theme, setTheme] = useState({
+    isLightTheme: true,
+    light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
+    dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
+  });
+
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContext.Provider value={theme}>
+        <Navbar />
+        <BookList />
+      </ThemeContext.Provider>
     </div>
   );
 }
